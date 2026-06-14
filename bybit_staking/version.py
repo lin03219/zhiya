2d9479159a1fa9610347ea12535409fbf90cc62c
User <user@bybit-staking.local>
2026-06-14 22:04:04 +0800

    v3.5 版本号升级

--- bybit_staking/version.py
+++ bybit_staking/version.py
@@ -1,6 +1,6 @@
 ﻿"""
 鐗堟湰淇℃伅
 """
-VERSION = "3.4"
+VERSION = "3.5"
 UPDATE_URL = "https://api.github.com/repos/lin03219/zhiya/releases/latest"
 RELEASES_URL = "https://github.com/lin03219/zhiya/releases"
